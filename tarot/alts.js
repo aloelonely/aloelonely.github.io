b8bf87aa5b77e574688bee23a200e5b4b19b1cbc
Ash Russell <arussell@suits-sandals.com>
2019-10-05 00:19:02 -0400

    upload

---
+++ tarot/alts.js
@@ -0,0 +1,82 @@
+function alts(slot,card){
+
+if (card==0){var alt="the fool";}
+if (card==1){var alt="the magician";}
+if (card==2){var alt="the high priestess";}
+if (card==3){var alt="the empress";}
+if (card==4){var alt="the emperor";}
+if (card==5){var alt="the heirophant";}
+if (card==6){var alt="the lovers";}
+if (card==7){var alt="the chariot";}
+if (card==8){var alt="strength";}
+if (card==9){var alt="the hermit";}
+if (card==10){var alt="the wheel of fortune";}
+if (card==11){var alt="justice";}
+if (card==12){var alt="the hanged man";}
+if (card==13){var alt="death";}
+if (card==14){var alt="temperance";}
+if (card==15){var alt="the devil";}
+if (card==16){var alt="the tower";}
+if (card==17){var alt="the star";}
+if (card==18){var alt="the moon";}
+if (card==19){var alt="the sun";}
+if (card==20){var alt="judgement";}
+if (card==21){var alt="the world";}
+if (card==22){var alt="the ace of wands";}
+if (card==23){var alt="two wands";}
+if (card==24){var alt="three wands";}
+if (card==25){var alt="four wands";}
+if (card==26){var alt="five wands";}
+if (card==27){var alt="six wands";}
+if (card==28){var alt="seven wands";}
+if (card==29){var alt="eight wands";}
+if (card==30){var alt="nine wands";}
+if (card==31){var alt="ten wands";}
+if (card==32){var alt="the page of wands";}
+if (card==33){var alt="the knight of wands";}
+if (card==34){var alt="the queen of wands";}
+if (card==35){var alt="the king of wands";}
+if (card==36){var alt="the ace of swords";}
+if (card==37){var alt="two swords";}
+if (card==38){var alt="three swords";}
+if (card==39){var alt="four swords";}
+if (card==40){var alt="five swords";}
+if (card==41){var alt="six swords";}
+if (card==42){var alt="seven swords";}
+if (card==43){var alt="eight swords";}
+if (card==44){var alt="nine swords";}
+if (card==45){var alt="ten swords";}
+if (card==46){var alt="the page of swords";}
+if (card==47){var alt="the knight of swords";}
+if (card==48){var alt="the queen of swords";}
+if (card==49){var alt="the king of swords";}
+if (card==50){var alt="the ace of cups";}
+if (card==51){var alt="two cups";}
+if (card==52){var alt="three cups";}
+if (card==53){var alt="four cups";}
+if (card==54){var alt="five cups";}
+if (card==55){var alt="six cups";}
+if (card==56){var alt="seven cups";}
+if (card==57){var alt="eight cups";}
+if (card==58){var alt="nine cups";}
+if (card==59){var alt="ten cups";}
+if (card==60){var alt="the page of cups";}
+if (card==61){var alt="the knight of cups";}
+if (card==62){var alt="the queen of cups";}
+if (card==63){var alt="the king of cups";}
+if (card==64){var alt="the ace of pentacles";}
+if (card==65){var alt="two pentacles";}
+if (card==66){var alt="three pentacles";}
+if (card==67){var alt="four pentacles";}
+if (card==68){var alt="five pentacles";}
+if (card==69){var alt="six pentacles";}
+if (card==70){var alt="seven pentacles";}
+if (card==71){var alt="eight pentacles";}
+if (card==72){var alt="nine pentacles";}
+if (card==73){var alt="ten pentacles";}
+if (card==74){var alt="the page of pentacles";}
+if (card==75){var alt="the knight of pentacles";}
+if (card==76){var alt="the queen of pentacles";}
+if (card==77){var alt="the king of pentacles";}
+return alt;  
+  }
